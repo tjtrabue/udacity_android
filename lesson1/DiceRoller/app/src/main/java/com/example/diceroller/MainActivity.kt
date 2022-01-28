7c85d28a10854ceb4c33e424eb0339d04ec66a25
Tom Trabue <tom.trabue@ramseysolutions.com>
2022-01-28 13:13:53 -0600

    Finish l1 e12: hello android!

--- lesson1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ lesson1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+// AppCompatActivity is a Kotlin subclass of Android that provides, and should
+// always be extended by any custom Activities in order to support the max
+// number of Android OS's.
 class MainActivity : AppCompatActivity() {
+    // Android apps don't have a "main" method. Instead, the main activity is
+    // defined in the activity's manifest XML file.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
