7285f1cb416f5846327080429cdabe81bcd9fb8e
Tom Trabue <tom.trabue@ramseysolutions.com>
2022-01-28 16:31:14 -0600

    Finish s1 e21: change dice image on roll

--- lesson1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ lesson1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
@@ -22,8 +22,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        var resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
