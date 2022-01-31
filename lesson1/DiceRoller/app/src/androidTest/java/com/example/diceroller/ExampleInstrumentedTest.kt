817f9144227b567826b5f71ab60a9208e142b21d
Tom Trabue <tom.trabue@ramseysolutions.com>
2022-01-31 13:03:08 -0600

    Finish s1 e28: srcCompat for vector drawables

--- lesson1/DiceRoller/app/src/androidTest/java/com/example/diceroller/ExampleInstrumentedTest.kt
+++ lesson1/DiceRoller/app/src/androidTest/java/com/example/diceroller/ExampleInstrumentedTest.kt
@@ -19,6 +19,9 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.diceroller", appContext.packageName)
+        assertEquals(
+            "com.example.diceroller",
+            appContext.packageName
+        )
     }
 }
